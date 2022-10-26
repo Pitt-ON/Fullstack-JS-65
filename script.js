897b583938b4f7c9e52be644b1743ade76e2e474
Pitt-ON <sekpe84@gmail.com>
2022-10-26 11:58:14 +0300

    add operator

--- script.js
+++ script.js
@@ -1,3 +1,45 @@
 "use strict"
 
-console.log(12)
+// const shouldRenew = confirm("Хотите продлить подписку?");
+// console.log(shouldRenew);
+
+
+// const quantity = prompt("Введите количество товаров");
+// console.log (quantity);
+
+
+// const age = 20;
+// const type = age >= 18 ? "adult" : "child";
+// console.log(type);
+
+
+// const num1 = 20;
+// const num2 = 10;
+// const biggerNumber = num1 > num2 ? num1 : num2;
+
+// console.log(biggerNumber);
+
+
+// let cost;
+// const subscription = "pro";
+
+// switch (subscription) {
+//   case "free":
+//     cost = 0;
+//     break;
+
+//   case "pro":
+//     cost = 100;
+//     break;
+
+//   case "premium":
+//     cost = 500;
+//     break;
+
+//   default:
+//     console.log("Invalid subscription type");
+// }
+
+// console.log(cost);
+
+
