1e3eaeee5262354c54ccd0de83d5f9c49a54be7a
Pitt-ON <sekpe84@gmail.com>
2022-11-15 14:44:23 +0200

    add 12

--- sсript-4.js
+++ sсript-4.js
@@ -1,7 +1,249 @@
-function makePizza() {
-  return "Your pizza is being prepared, please wait.";
-}
-// Change code below this line
+// 1 Виклик і вивід функції
+// function makePizza() {
+//   return "Your pizza is being prepared, please wait.";
+// }
+// // Change code below this line
 
-const result = null;
-const pointer = null;
+// const result = makePizza();
+// console.log(result);
+// const pointer = makePizza;
+
+
+// 2 Додавання callback до функціі
+// function deliverPizza(pizzaName) {
+//   return `Delivering ${pizzaName} pizza.`;
+// }
+
+// function makePizza(pizzaName) {
+//   return `Pizza ${pizzaName} is being prepared, please wait...`;
+// }
+
+// // Chande code below this line
+// function makeMessage(pizzaName, callback) {
+//   return callback(pizzaName);
+// }
+// console.log(makeMessage("Royal Grand", makePizza));
+// console.log(makeMessage("Ultracheese", deliverPizza));
+
+
+// 3 Передача інлайн колбек-функції
+// function makePizza(pizzaName, callback) {
+//   console.log(`Pizza ${pizzaName} is being prepared, please wait...`);
+//   callback(pizzaName);
+// }
+
+// makePizza("Royal Grand", function deliverPizza(pizzaName) {
+//   console.log(`Delivering pizza ${pizzaName}.`);
+// });
+// // Change code below this line
+
+// makePizza("Ultracheese", function eatPizza(pizzaName) {
+//     console.log(`Eating pizza ${pizzaName}`)
+// });
+
+
+// 4 Рефакторинг методу
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//     order(pizzaName, onSuccess, onError) {
+//         for (let nameElem of pizzaPalace.pizzas) {
+//             if (nameElem === pizzaName) {
+//                 return onSuccess(pizzaName);
+//           }
+//         }
+//         return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+//   },
+// };
+// // Change code above this line
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return `Your order is accepted. Cooking pizza ${pizzaName}.`;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// pizzaPalace.order("Smoked", makePizza, onOrderError);
+// pizzaPalace.order("Four meats", makePizza, onOrderError);
+// pizzaPalace.order("Big Mike", makePizza, onOrderError);
+// pizzaPalace.order("Vienna", makePizza, onOrderError);
+
+
+// 5 Метод перебирання масиву forEach
+// function calculateTotalPrice(orderedItems) {
+//   let totalPrice = 0;
+//   // Change code below this line
+
+// //   for (let i = 0; i < orderedItems.length; i += 1) {
+// //     totalPrice += orderedItems[i];
+// //   }
+    
+//     orderedItems.forEach(function (item) {
+//         totalPrice += item;
+//     });
+
+//   // Change code above this line
+//   return totalPrice;
+// }
+// calculateTotalPrice([12, 85, 37, 4]);
+// calculateTotalPrice([164, 48, 291]);
+// calculateTotalPrice([412, 371, 94, 63, 176]);
+
+
+// 6 Фільтрація масиву чисел
+// function filterArray(numbers, value) {
+//   const filteredNumbers = [];
+//   // Change code below this line
+
+// //   for (let i = 0; i < numbers.length; i += 1) {
+// //     if (numbers[i] > value) {
+// //       filteredNumbers.push(numbers[i]);
+// //     }
+// //     }
+    
+//     numbers.forEach(function (number) {
+//         if (number > value) {
+//             filteredNumbers.push(number);
+//     }
+// })
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+// filterArray([12, 24, 8, 41, 76], 20);
+
+
+// 7 Спільні елементи. Порівняння двох масивів
+// function getCommonElements(firstArray, secondArray) {
+//   const commonElements = [];
+//   // Change code below this line
+
+// //   for (let i = 0; i < firstArray.length; i += 1) {
+// //     if (secondArray.includes(firstArray[i])) {
+// //       commonElements.push(firstArray[i]);
+// //     }
+// //     }
+    
+//     firstArray.forEach(function (number) {
+//         if (secondArray.includes(number)) {
+//             commonElements.push(number);
+//     }
+// })
+
+//   return commonElements;
+//   // Change code above this line
+// }
+// getCommonElements([1, 2, 3], [2, 4]);
+// getCommonElements([1, 2, 3], [2, 1, 17, 19]);
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]);
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]);
+// getCommonElements([1, 2, 3], [10, 20, 30]);
+
+
+// 8 Рефакторінг функції до стрілочної =>
+// // Change code below this line
+
+// // function calculateTotalPrice(quantity, pricePerItem)
+
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   // Change code above this line
+//   return quantity * pricePerItem;
+// }
+// calculateTotalPrice(5, 100);
+// calculateTotalPrice(8, 60);
+// calculateTotalPrice(3, 400);
+
+
+// 9 Неявне повернення (відсутні фігурні дужки)
+// Change code below this line
+// const calculateTotalPrice = (quantity, pricePerItem) => {
+//   return quantity * pricePerItem;
+// };
+// Change code above this line
+// const calculateTotalPrice = (quantity, pricePerItem) => quantity * pricePerItem;
+// calculateTotalPrice(5, 100);
+// calculateTotalPrice(8, 60);
+// calculateTotalPrice(3, 400);
+
+
+// 10 Заміна callback функції на стрілочну
+//  // Change code below this line
+// // function calculateTotalPrice(orderedItems)
+// const calculateTotalPrice = (orderedItems) => {
+//   let totalPrice = 0;
+
+//   orderedItems.forEach((item) => {
+//     totalPrice += item;
+//   });
+
+//   return totalPrice;
+// }
+// // Change code above this line
+// calculateTotalPrice([12, 85, 37, 4]);
+// calculateTotalPrice([164, 48, 291]);
+// calculateTotalPrice([412, 371, 94, 63, 176]);
+
+
+// 11 ФІЛЬТРАЦІЯ МАСИВУ ЧИСЕЛ 2.0
+// Change code below this line
+// const filterArray = (numbers, value) => {
+//   const filteredNumbers = [];
+
+//   numbers.forEach((number) => {
+//     if (number > value) {
+//       filteredNumbers.push(number);
+//     }
+//   });
+
+//   // Change code above this line
+//   return filteredNumbers;
+// }
+// filterArray([1, 2, 3, 4, 5], 3);
+// filterArray([1, 2, 3, 4, 5], 4);
+// filterArray([1, 2, 3, 4, 5], 5);
+// filterArray([12, 24, 8, 41, 76], 38);
+
+
+// // 12 СПІЛЬНІ ЕЛЕМЕНТИ 2.0
+// // Change code below this line
+// const getCommonElements = (firstArray, secondArray) => {
+//   const commonElements = [];
+
+//   firstArray.forEach((element) => {
+//     if (secondArray.includes(element)) {
+//       commonElements.push(element);
+//     }
+//   });
+
+//   // Change code above this line
+//   return commonElements;
+// }
+// getCommonElements([1, 2, 3], [2, 4]);
+// getCommonElements([1, 2, 3], [2, 1, 17, 19]);
+// getCommonElements([24, 12, 27, 3], [12, 8, 3, 36, 27]);
+// getCommonElements([10, 20, 30, 40], [4, 30, 17, 10, 40]);
+// getCommonElements([1, 2, 3], [10, 20, 30]);
+
+
+// 13 Чиста функція (pure function)
+function changeEven(numbers, value) {
+  // Change code below this line
+  for (let i = 0; i < numbers.length; i += 1) {
+    if (numbers[i] % 2 === 0) {
+      numbers[i] = numbers[i] + value;
+    }
+  }
+  // Change code above this line
+}
+changeEven([1, 2, 3, 4, 5], 10);
+changeEven([2, 8, 3, 7, 4, 6], 10);
+changeEven([17, 24, 68, 31, 42], 100);
+changeEven([44, 13, 81, 92, 36, 54], 100);
