a69a137992b468afdecf2f4f4ac2324192a5450e
Pitt-ON <sekpe84@gmail.com>
2022-11-19 13:55:11 +0200

    35

--- sсript-4.js
+++ sсript-4.js
@@ -431,10 +431,237 @@
 // };
 // Change code above this line
 
+// const users = [
+//   {
+//     name: "Moore Hensley",
+//     email: "moorehensley@indexia.com",
+//     eyeColor: "blue",
+//     friends: ["Sharron Pace"],
+//     isActive: false,
+//     balance: 2811,
+//     gender: "male",
+//     age: 37
+//   },
+//   {
+//     name: "Sharlene Bush",
+//     email: "sharlenebush@tubesys.com",
+//     eyeColor: "blue",
+//     friends: ["Briana Decker", "Sharron Pace"],
+//     isActive: true,
+//     balance: 3821,
+//     gender: "female",
+//     age: 34
+//   },
+//   {
+//     name: "Ross Vazquez",
+//     email: "rossvazquez@xinware.com",
+//     eyeColor: "green",
+//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
+//     isActive: false,
+//     balance: 3793,
+//     gender: "male",
+//     age: 24
+//   },
+//   {
+//     name: "Elma Head",
+//     email: "elmahead@omatom.com",
+//     eyeColor: "green",
+//     friends: ["Goldie Gentry", "Aisha Tran"],
+//     isActive: true,
+//     balance: 2278,
+//     gender: "female",
+//     age: 21
+//   },
+//   {
+//     name: "Carey Barr",
+//     email: "careybarr@nurali.com",
+//     eyeColor: "blue",
+//     friends: ["Jordan Sampson", "Eddie Strong"],
+//     isActive: true,
+//     balance: 3951,
+//     gender: "male",
+//     age: 27
+//   },
+//   {
+//     name: "Blackburn Dotson",
+//     email: "blackburndotson@furnigeer.com",
+//     eyeColor: "brown",
+//     friends: ["Jacklyn Lucas", "Linda Chapman"],
+//     isActive: false,
+//     balance: 1498,
+//     gender: "male",
+//     age: 38
+//   },
+//   {
+//     name: "Sheree Anthony",
+//     email: "shereeanthony@kog.com",
+//     eyeColor: "brown",
+//     friends: ["Goldie Gentry", "Briana Decker"],
+//     isActive: true,
+//     balance: 2764,
+//     gender: "female",
+//     age: 39
+//   }
+// ];
 
-// 25 Функція поверталє масив друзів всіх користувачів (властивість friends).
+
+// 25 flatMap(). Функція поверталє масив друзів всіх користувачів (властивість friends).
 // Change code below this line
-const getFriends = (users) => {
-   
-};
+// const getFriends = (users) => {
+//   // Створюємо змінну і розгладжуємо масив
+//   const allFriends = users.flatMap(user => user.friends);
+//   // Створюємо змінну, виконуемо фільтрацію
+//   const uniqFriends = allFriends.filter((friend, index, self) =>
+//     // перевіряємо на повторення елементів
+//     self.indexOf(friend) === index);
+//   return uniqFriends;
+// };
 // Change code above this line
+
+
+// 26 Функція повертає масив активних користувачів, значення властивості
+//  isActive яких - true.
+// Change code below this line
+// const getActiveUsers = (users) => {
+//   return users.filter(user => user.isActive);
+// };
+// Change code above this line
+
+
+// 27 Функція повертає масив неактивних користувачів, значення властивості
+//  isActive яких - false.
+// Change code below this line
+// const getInactiveUsers = (users) => {
+//   return users.filter(user => !user.isActive)
+// };
+// Change code above this line
+
+
+// 28 Використовуючи метод find(), доповни код таким чином, щоб:
+
+// У змінній bookWithTitle утворився об'єкт книги, назва якої (властивість title)
+//  збігається зі значенням змінної BOOK_TITLE.
+// У змінній bookByAuthor утворився об'єкт книги, автор якої (властивість author)
+//  збігається зі значенням змінної AUTHOR.
+// const books = [
+//   {
+//     title: 'The Last Kingdom',
+//     author: 'Bernard Cornwell',
+//     rating: 8.38,
+//   },
+//   {
+//     title: 'Beside Still Waters',
+//     author: 'Robert Sheckley',
+//     rating: 8.51,
+//   },
+//   {
+//     title: 'The Dream of a Ridiculous Man',
+//     author: 'Fyodor Dostoevsky',
+//     rating: 7.75,
+//   },
+//   { title: 'Redder Than Blood', author: 'Tanith Lee', rating: 7.94 },
+// ];
+// const BOOK_TITLE = 'The Dream of a Ridiculous Man';
+// const AUTHOR = 'Robert Sheckley';
+// // Change code below this line
+
+// const bookWithTitle = books.find(book => book.title === BOOK_TITLE);
+// const bookByAuthor = books.find(book => book.author === AUTHOR);
+
+
+// 29 Функція повертає об'єкт користувача, пошта якого
+// (властивість email) збігається зі значенням параметра email.
+// Change code below this line
+// const getUserWithEmail = (users, email) => {
+//    return users.find(user => user.email === email)
+// };
+// Change code above this line
+
+
+// 30 Використання методу every(callback). Перевірка масиву на парність чи непарність.
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change code below this line
+
+// const eachElementInFirstIsEven = firstArray.every(value => value % 2 === 0);
+// const eachElementInFirstIsOdd = firstArray.every(value => value % 2 !== 0);
+
+// const eachElementInSecondIsEven = secondArray.every(value => value % 2 === 0);
+// const eachElementInSecondIsOdd = secondArray.every(value => value % 2 !== 0);
+
+// const eachElementInThirdIsEven = thirdArray.every(value => value % 2 ===0);
+// const eachElementInThirdIsOdd = thirdArray.every(value => value % 2 !== 0);
+
+
+// 31 Функція перевіряє, чи всі користувачі зараз активні (властивість isActive)
+//  і повертає true або false.
+// Change code below this line
+// const isEveryUserActive = (users) => {
+//    return users.every(user => user.isActive)
+// };
+// Change code above this line
+
+
+// 32 Використання методу some().
+//  Перевірка масиву на наявність парність і непарних чисел.
+// const firstArray = [26, 94, 36, 18];
+// const secondArray = [17, 61, 23];
+// const thirdArray = [17, 26, 94, 61, 36, 23, 18];
+// // Change below this line
+
+// const anyElementInFirstIsEven = firstArray.some(value => value % 2 === 0);
+// const anyElementInFirstIsOdd = firstArray.some(value => value % 2 !== 0);
+
+// const anyElementInSecondIsEven = secondArray.some(value => value % 2 === 0);
+// const anyElementInSecondIsOdd = secondArray.some(value => value % 2 !== 0);
+
+// const anyElementInThirdIsEven = thirdArray.some(value => value % 2 === 0);
+// const anyElementInThirdIsOdd = thirdArray.some(value => value % 2 !== 0);
+
+
+// 33 Функція перевіряє, наявність активних користувачів (властивість isActive)
+//  і повертала true або false.
+// Change code below this line
+// const isAnyUserActive = users => {
+//    return users.some(user => user.isActive)
+// };
+// Change code above this line
+
+
+// 34 Метод reduce(callback, initialValue).
+// Підрахунку загального ігрового часу, проведеного в іграх.
+// const players = {
+//   mango: 1270,
+//   poly: 468,
+//   ajax: 710,
+//   kiwi: 244
+// };
+// // 1. Отримаємо масив зі значеннями об'єкта
+// const playtimes = Object.values(players); // [1270, 468, 710, 244]
+// // Change code below this line
+// // 2. Суммуємо значеня з масива
+// const totalPlayTime = playtimes.reduce((previousValue, player) => {
+//   return previousValue + player;
+// }, 0);
+// console.log(totalPlayTime);
+// // Change code above this line
+// // 3. Вираховуємо середнє значення
+// const averagePlayTime = totalPlayTime / playtimes.length;
+
+
+// 35 Розрахунок середнього часу, проведеного в одній грі для кожного гравця,
+// і отримання загальної суми цих значень.
+// const players = [
+//   { name: "Mango", playtime: 1270, gamesPlayed: 4 },
+//   { name: "Poly", playtime: 469, gamesPlayed: 2 },
+//   { name: "Ajax", playtime: 690, gamesPlayed: 3 },
+//   { name: "Kiwi", playtime: 241, gamesPlayed: 1 },
+// ];
+// // Change code below this line
+
+// const totalAveragePlaytimePerGame = players.reduce((totalTime, player) => {
+//   return totalTime + player.playtime / player.gamesPlayed;
+// }, 0);
+// console.log(totalAveragePlaytimePerGame);
+
