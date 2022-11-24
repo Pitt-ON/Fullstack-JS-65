3c891451b44df3cf9956cf54913066662ef03c71
Pitt-ON <sekpe84@gmail.com>
2022-11-24 15:12:45 +0200

    end 15

--- script-5.js
+++ script-5.js
@@ -222,35 +222,174 @@
 
 // 11 Напиши клас StringBuilder, який приймає один параметр initialValue - довільний
 // рядок, який записується у властивість value об'єкта, що створюється.
-class StringBuilder {
-    constructor(value) {
-        this.value = value;
-    }
+// class StringBuilder {
+//     constructor(initialValue) {
+//         this.value = initialValue;
+//     }
 
-    getValue() {
-        this.value;
-    }
+//     getValue() {
+//        return this.value;
+//     }
 
-    padEnd(str) {
-        this.value + str;
-    }
+//     padEnd(str) {
+//        this.value = this.value + str;
+//     }
 
-    padStart(str) {
-        this.value - str;
-    }
+//     padStart(str) {
+//        this.value = str + this.value;
+//     }
 
-    padBoth(str) {
-        this.value + str;
-        this.value - str;
-    }
-}
+//     padBoth(str) {
+//         this.value = str + this.value + str;
+//     }
+// }
 
-// Change code above this line
-const builder = new StringBuilder(".");
-console.log(builder.getValue()); // "."
-builder.padStart("^");
-console.log(builder.getValue()); // "^."
-builder.padEnd("^");
-console.log(builder.getValue()); // "^.^"
-builder.padBoth("=");
-console.log(builder.getValue()); // "=^.^="
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+
+// 12 Виконай рефакторинг класу Car таким чином, щоб властивість brand була
+// приватною, і додай два методи для публічного інтерфейсу, для читання і зміни
+// цієї властивості.
+// class Car {
+//   // Change code below this line
+//     #brand;
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.model = model;
+//     this.price = price;
+//     }
+    
+//     getBrand() {
+//        return this.#brand;
+//     }
+
+//     changeBrand(newBrand) {
+//         this.#brand = newBrand;
+//     }
+
+//   // Change code above this line
+// }
+// const audi = new Car({brand: "Audi", model: "Q3", price: 36000});
+// console.log(audi);
+// const bmw = new Car({brand: "BMW", model: "X5", price: 58900});
+// console.log(bmw);
+// const nissan = new Car({brand: "Nissan", model: "Murano", price: 31700});
+// console.log(nissan);
+
+
+// 13 Виконай рефакторинг класу Storage, зробивши властивість items приватною.
+// class Storage {
+//   // Change code below this line
+//     #items;
+//   constructor(items) {
+//     this.#items = items;
+//   }
+
+//   getItems() {
+//     return this.#items;
+//   }
+
+//   addItem(newItem) {
+//     this.#items.push(newItem);
+//   }
+
+//   removeItem(itemToRemove) {
+//     this.#items = this.#items.filter(item => item !== itemToRemove);
+//   }
+// }
+
+// // Change code above this line
+// const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+// storage.addItem("Droid");
+// console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+// storage.removeItem("Prolonger");
+// console.log(storage.getItems()); // ["Нанитоиды", "Антигравитатор", "Droid"]
+
+
+// 14 Виконай рефакторинг класу StringBuilder, зробивши властивість value приватною.
+// class StringBuilder {
+//   // Change code below this line
+//     #value;
+//   constructor(initialValue) {
+//     this.#value = initialValue;
+//   }
+
+//   getValue() {
+//     return this.#value;
+//   }
+
+//   padEnd(str) {
+//     this.#value += str;
+//   }
+
+//   padStart(str) {
+//     this.#value = str + this.#value;
+//   }
+
+//   padBoth(str) {
+//     this.padStart(str);
+//     this.padEnd(str);
+//   }
+// }
+
+// // Change code above this line
+// const builder = new StringBuilder(".");
+// console.log(builder.getValue()); // "."
+// builder.padStart("^");
+// console.log(builder.getValue()); // "^."
+// builder.padEnd("^");
+// console.log(builder.getValue()); // "^.^"
+// builder.padBoth("=");
+// console.log(builder.getValue()); // "=^.^="
+
+
+// 15 Виконай рефакторинг класу Car.Зроби властивості model і price приватними,
+// а також #brand.Стандартизуй публічний інтерфейс класу, замінивши вже оголошені
+// методи на гетери та сетери brand, model і price, для взаємодії з приватними
+// властивостями.
+// class Car {
+//   // Change code below this line
+//     #brand;
+//     #model;
+//     #price;
+
+//   constructor({ brand, model, price }) {
+//     this.#brand = brand;
+//     this.#model = model;
+//     this.#price = price;
+//   }
+
+//   get brand() {
+//     return this.#brand;
+//   }
+
+//   set brand(newBrand) {
+//     this.#brand = newBrand;
+//   }
+
+//   get model() {
+//     return this.#model;
+//   }
+
+//   set model(newModel) {
+//     this.#model = newModel;
+//   }
+
+//   get price() {
+//     return this.#price;
+//   }
+
+//   set price(newPrice) {
+//     this.#price = newPrice;
+//   }
+//   // Change code above this line
+// }
